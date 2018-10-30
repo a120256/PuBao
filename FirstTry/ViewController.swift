415fefcdc3e21dd329fbbe42e6fa817eeb46409b
帥氣阿剛 <alex_wang@tinggangwangde-macbook-pro.local>
2018-10-30 19:37:00 +0800

    Initial Commit

---
+++ FirstTry/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstTry
+//
+//  Created by 帥氣阿剛 on 2018/10/30.
+//  Copyright © 2018 帥氣阿剛. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
